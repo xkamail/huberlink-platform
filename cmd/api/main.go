8bd8ebf9c8ed04ccd981c577a141c957442a2268
Pakorn Sangpeth <camel4882@gmail.com>
2023-02-20 08:37:16 +0700

    feat: dotconfig

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/xkamail/dotconfig"
+)
+
+type Config struct {
+	Port string `env:"PORT" default:"8080"`
+}
+
+var cfg Config
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+func run() error {
+	if err := dotconfig.Load(&cfg, "./.env"); err != nil {
+		return err
+	}
+	return nil
 }
