9611d512db9174dc2186e434290b5b58cc77c9c6
Pakorn Sangpeth <camel4882@gmail.com>
2023-02-27 22:51:01 +0700

    fix: title

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,28 +1,28 @@
-import { cn } from '@/lib/utils'
+import {cn} from '@/lib/utils'
 import '@/styles/output.css'
-import { Inter } from '@next/font/google'
+import {Inter} from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+        {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head>
-        <title>HuberLink</title>
-      </head>
-      <body
-        className={cn`min-h-screen text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50 ${inter.className}`}
-      >
+        <head>
+            <meta charSet="utf-8"/>
+        </head>
+        <body
+            className={cn`min-h-screen text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50 ${inter.className}`}
+        >
         <div className="flex min-h-screen flex-col">{children}</div>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
