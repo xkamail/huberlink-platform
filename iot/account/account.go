c51fa005e59dc9bdc5b50e5558b3bcff1a412a90
Pakorn Sangpeth <camel4882@gmail.com>
2023-02-28 21:14:44 +0700

    feat: find user

--- iot/account/account.go
+++ iot/account/account.go
@@ -1,8 +1,31 @@
 package account
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/xkamail/huberlink-platform/pkg/pgctx"
+	"github.com/xkamail/huberlink-platform/pkg/uierr"
+)
+
+var (
+	ErrNotFound = uierr.NotFound("user not found")
+)
+
 type User struct {
 	ID       int64
 	Username string
 	Password string
 	Email    string
+}
+
+// Find user by id
+func Find(ctx context.Context, userID int64) (*User, error) {
+	rows, err := pgctx.Query(ctx, ``)
+	if err != nil {
+		return nil, err
+	}
+	pgx.CollectOneRow(rows, pgx.RowToStructByPos[User])
+
 }
