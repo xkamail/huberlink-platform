d0bf94939d5d5b783758718383fa9aa5111aef84
Pakorn Sangpeth <camel4882@gmail.com>
2023-02-28 21:13:40 +0700

    feat: write response

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,7 +1,23 @@
 package api
 
+import (
+	"net/http"
+
+	"github.com/xkamail/huberlink-platform/pkg/uierr"
+)
+
 type Format struct {
+	Code    uierr.Code  `json:"code"`
 	Success bool        `json:"success"`
 	Data    interface{} `json:"data"`
 	Errors  []any       `json:"errors"`
+	Message string      `json:"message"`
+}
+
+func WriteError(w http.ResponseWriter, err error) {
+	
+}
+
+func Write[T any](w http.ResponseWriter, d T) {
+
 }
