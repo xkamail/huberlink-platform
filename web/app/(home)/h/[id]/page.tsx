b326bc558735466e9cefbc18c1301e0177af74fe
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-02 23:41:23 +0700

    style: home page

--- web/app/(home)/h/[id]/page.tsx
+++ web/app/(home)/h/[id]/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Card from '@/components/ui/card'
 import HomeService from '@/services/HomeService'
 const fetchData = async (id: string) => {
   return await HomeService.findById(id)
@@ -6,8 +7,35 @@ const fetchData = async (id: string) => {
 
 const HomePage = ({ params }: { params: { id: string } }) => {
   return (
-    <div>
-      <p></p>
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-12">
+        <div className="rounded-lg shadow bg-indigo-600 text-slate-100 p-4">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img
+                className="h-10 w-10 rounded-full"
+                src="https://via.placeholder.com/400"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <div className="text-base font-medium ">Pakorn Sangpeth</div>
+            <div>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the standard dummy text ever since
+              the 1500s, when an unknown printer took a galley of type and
+              scrambled it to make a type specimen book.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-6">
+        <Card />
+      </div>
+      <div className="col-span-6">
+        <Card />
+      </div>
     </div>
   )
 }
--- web/components/navigation/TopNavigation.tsx
+++ web/components/navigation/TopNavigation.tsx
@@ -7,7 +7,7 @@ type IProps = {
 const TopNavigation = ({ title, leftContent }: IProps) => {
   const loading = useHomeSelector((s) => s.isLoading)
   return (
-    <div className="rounded-lg bg-gradient-to-r from-blue-500  to-indigo-600 p-4 mb-4 text-white">
+    <div className="rounded-lg shadow bg-white p-4 mb-4">
       <div className="flex justify-between items-center">
         <div>{leftContent}</div>
         <h1 className="text-xl md:text-2xl font-bold text-right ">
