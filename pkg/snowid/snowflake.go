1fb125687677583fdb75c2310355cef6a4b05781
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-02 19:42:05 +0700

    fix: unmarshal snowflake

--- pkg/snowid/snowflake.go
+++ pkg/snowid/snowflake.go
@@ -9,17 +9,28 @@ import (
 
 type ID int64
 
-func (i ID) MarshalJSON() ([]byte, error) {
+func (i *ID) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+	id, err := snowflake.ParseString(s)
+	if err != nil {
+		return err
+	}
+	*i = ID(id.Int64())
+	return nil
+}
+
+func (i *ID) MarshalJSON() ([]byte, error) {
 	return json.Marshal(fmt.Sprint(i))
 }
 
-func (i ID) Int() int64 {
-	return int64(i)
+func (i *ID) Int() int64 {
+	return int64(*i)
 }
 
 const Zero = ID(0)
-
-var _ json.Marshaler = ID(0)
 
 var node, _ = snowflake.NewNode(1)
 
--- pkg/snowid/snowflake_test.go
+++ pkg/snowid/snowflake_test.go
@@ -1,12 +1,24 @@
 package snowid
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGen(t *testing.T) {
+	t.Parallel()
 	t.Run("Gen", func(t *testing.T) {
 		a := Gen()
 		t.Log(a)
+	})
+	t.Run("json string unmarshal", func(t *testing.T) {
+		ran := Gen()
+		var a ID
+		err := json.Unmarshal([]byte(fmt.Sprintf(`"%v"`, ran)), &a)
+		assert.NoError(t, err)
+		assert.Equal(t, ran, a)
 	})
 }
