286a2ac4004a20f812aa2f7b50716b3410368eb0
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-17 12:39:18 +0700

    feat: extract topic from topic string

--- hardware/irremote/esp/esp.ino
+++ hardware/irremote/esp/esp.ino
@@ -12,14 +12,11 @@
 
 #define DEVICE_TOKEN "eOq830I6_QRpE3W-S1F_hx1Zj9BGczeVOLf4ieW9J09KWIHq5AElaMxm3KhFx6bt8zSOjJwUUCW9TQvml4wTI7g2PiBX8mdaNCHdA97JrREoCIvYN8BSnDHVMCJB6bnfv4iMKxte8OehrpyJehC9TKN1gt774w6_yRMsOUVU0-HAohyoJ5ICfWEGngkhEW5HnQwbmybdnuPVhYSgZ-z8uUqQpSE2UXMuUAZ0Z-5_u3R0bgcwyP9Z2SjHcndvxvGFQPCoaq8pRi3wMsFMaZvwfqNMY86D17t1AL7cTJ2uqJWwsROU4G0wp2rr3ciZZVMo0yXdb4PT4DqCvEjtFlYEMH57eaWUN-lyw6WRMnqzBSLwuFbtvRDFJ7HAvyrxbv_9-zm8w_7n3X-tcvasIVchAxXjwkzOAJZe4qy7iYwxRxEps5rO3JENllWlS0bR5LpYir8veiAT5Yyp7Ji6xIv-cecV7S9mJ6megLTYeeh5g4cky5B1IL6MflRwNOsNsNAk2nFQ8mnqL2hZXlQQvVAWkz-8KHsZG9AGpiiOK6qnzYJMDqVJ7VsG_QwmjPCz2yk4a2wasKXnPCcIor0rzzlCQoQLLcNkv_sOofPPM9gK1aZ9_s1RluVxWT8eNdBjW4acN6MXUWjyYbOZmYuKBOgQMMGNsj4="
 
-const char *DEVICE_ID = "1633177370523340800";
-// what wrong of this line ?
-const char topicConfig[100], topicState[100], topicExecute[100];
-sprintf(topic, "devices/%s", DEVICE_ID);
-sprintf(topicConfig, "devices/%s", DEVICE_ID);
-sprintf(topicConfig, "devices/%s", DEVICE_ID);
-
-
+#define DEVICE_ID "1633177370523340800"
+// const topic
+#define TOPIC_REPORT "huberlink/" STRING(DEVICE_ID) "/thing/report";
+#define TOPIC_HEARTBEAT "huberlink/" STRING(DEVICE_ID) "/thing/heartbeat";
+#define TOPIC_EXECUTE "huberlink/" STRING(DEVICE_ID) "/thing/execute";
 
 WiFiManager wm;
 WiFiClient espClient;
@@ -30,19 +27,24 @@ const byte numChars = 32;
 char receivedChars[numChars];
 boolean newData = false;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(115200);
 
   // automatically connect using saved credentials if they exist
   // If connection fails it starts an access point with the specified name
-  if (wm.autoConnect("IRRemote HuberLink")) {
+  if (wm.autoConnect("IRRemote HuberLink"))
+  {
     Serial.println("connected...yeey :)");
-  } else {
+  }
+  else
+  {
     Serial.println("Configportal running");
   }
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -51,13 +53,18 @@ void setup() {
   client.setCallback(MQTTHandler);
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
-    if (client.connect(DEVICE_ID, "test", "test")) {
+    if (client.connect(DEVICE_ID, "test", "test"))
+    {
       Serial.println("connected");
       client.subscribe("/ESP/LED");
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -75,49 +82,60 @@ void loop() {
   sendNewData();
 }
 
-void serialEvent() {
+void serialEvent()
+{
   static boolean recvInProgress = false;
   static byte ndx = 0;
   char startMarker = '<';
   char endMarker = '>';
   char rc;
 
-  while (Serial.available() > 0 && newData == false) {
+  while (Serial.available() > 0 && newData == false)
+  {
     rc = Serial.read();
 
-    if (recvInProgress == true) {
-      if (rc != endMarker) {
+    if (recvInProgress == true)
+    {
+      if (rc != endMarker)
+      {
         receivedChars[ndx] = rc;
         ndx++;
-        if (ndx >= numChars) {
+        if (ndx >= numChars)
+        {
           ndx = numChars - 1;
         }
-      } else {
-        receivedChars[ndx] = '\0';  // terminate the string
+      }
+      else
+      {
+        receivedChars[ndx] = '\0'; // terminate the string
         recvInProgress = false;
         ndx = 0;
         newData = true;
       }
     }
 
-    else if (rc == startMarker) {
+    else if (rc == startMarker)
+    {
       recvInProgress = true;
     }
   }
 }
-void sendNewData() {
+void sendNewData()
+{
   //
-  if (newData == true) {
+  if (newData == true)
+  {
     Serial.print("This just in ... ");
     Serial.println(receivedChars);
     newData = false;
   }
 }
 
-void MQTTHandler(char *topic, byte *p, unsigned int length) {
+void MQTTHandler(char *topic, byte *p, unsigned int length)
+{
   Serial.print("Message: ");
   Serial.print(topic);
-  Serial.prinln("");
+  Serial.println("");
 
   String jsonStr = "";
   unsigned int i = 0;
@@ -125,34 +143,30 @@ void MQTTHandler(char *topic, byte *p, unsigned int length) {
     jsonStr += (char)p[i++];
   //
 
-  DynamicJsonDocument doc(1024);
-  deserializeJson(doc, json);
-
-  const char *sensor = doc["sensor"];
-  long time = doc["time"];
-  double latitude = doc["data"][0];
-  double longitude = doc["data"][1];
-  // check if topic start with /devices/{device_id}
-  // then check if topic is /devices/{device_id}/command
-  // then check if topic is /devices/{device_id}/config
-  // then check if topic is /devices/{device_id}/state
-
-  // check string has prefix devices/{device_id} or not
-  String topicStr = String(topic);
-  String prefix = String("devices/") + String(DEVICE_ID);
-  if (!topicStr.startsWith(prefix)) {
+  String _topicStr = String(topic);
+  String prefix = String("huberlink/") + String(DEVICE_ID);
+  if (!_topicStr.startsWith(prefix))
+  {
     Serial.println("Topic not match");
     return;
   }
+  // split topic from huberlink/device_id/topicname
+  String topicName = _topicStr.substring(prefix.length() + 1);
+  Serial.println(topicName);
+  if (topicName != "thing/execute")
+    return;
+  // do exuecute
 }
 
 unsigned long latestBeat = 0;
-void onHeartbeat() {
+void onHeartbeat()
+{
   // check if mqtt is connected and wifi also
   // then publish a topic into mqtt server
   // topic: /generic/{device_id}/heartbeat
   unsigned long currentMillis = millis();
-  if (currentMillis - latestBeat >= 5000) {
+  if (currentMillis - latestBeat >= 5000)
+  {
     latestBeat = currentMillis;
     Serial.println("beat~");
     char str[50];
@@ -161,7 +175,8 @@ void onHeartbeat() {
   }
 }
 
-void Publish(const char *topic, const char *payload) {
+void Publish(const char *topic, const char *payload)
+{
   // trim /
   while (topic[0] == '/')
     topic++;
