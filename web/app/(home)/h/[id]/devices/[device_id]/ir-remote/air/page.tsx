cbe4d82236b4fe8f52470425cb61abf047dd7414
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-22 12:11:01 +0700

    refactor: air manage page

--- web/app/(home)/h/[id]/devices/[device_id]/ir-remote/air/page.tsx
+++ web/app/(home)/h/[id]/devices/[device_id]/ir-remote/air/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { Icons } from '@/components/icons'
 import PageHeader from '@/components/ui/page-header'
 import { cn } from '@/lib/utils'
-import { MinusIcon, PlusIcon, SnowflakeIcon } from 'lucide-react'
 import Image from 'next/image'
 import { useState } from 'react'
 
@@ -34,55 +32,11 @@ const AirSettingPage = () => {
               className="w-24 h-24 mx-auto"
             />
           </div>
-          <div className="flex flex-row gap-4 justify-center items-center">
-            <div className="">
-              <MinusIcon className={btnClassName} />
-            </div>
-            <div>
-              <h1 className="text-6xl font-bold">25</h1>
-            </div>
-            <div>
-              <PlusIcon className={btnClassName} />
-            </div>
-          </div>
-          <div className="">
-            <div className="flex flex-row gap-4 justify-center items-center">
-              <ToggleButton
-                label={`Mode: ${mode}`}
-                loading={mode === 'loading'}
-                onToggle={toggleMode}
-              />
-              <a href="" className="p-4">
-                Speed: Auto
-              </a>
-            </div>
-          </div>
+          <div className="flex flex-row gap-4 justify-center items-center"></div>
+          <div className=""></div>
         </div>
       </div>
     </>
-  )
-}
-
-const ToggleButton = ({
-  onToggle,
-  loading,
-  label,
-}: {
-  onToggle: () => void
-  loading: boolean
-  label: string
-}) => {
-  return (
-    <button onClick={onToggle} className="p-4 gap-2">
-      <div className="bg-white transition-all  shadow cursor-pointer text-center rounded-full p-4 flex justify-center items-center group">
-        {loading ? (
-          <Icons.spin className="w-10 h-10 group-hover:text-slate-700 animate-spin" />
-        ) : (
-          <SnowflakeIcon className="w-10 h-10 group-hover:text-slate-700" />
-        )}
-      </div>
-      <p className="text-sm flex items-center">{label}</p>
-    </button>
   )
 }
 
