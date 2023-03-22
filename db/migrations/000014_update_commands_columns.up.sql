f1400866a33449186b42184544c709da81d86370
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-22 16:19:08 +0700

    fix: uint array type

--- db/migrations/000014_update_commands_columns.up.sql
+++ db/migrations/000014_update_commands_columns.up.sql
@@ -1,4 +1,4 @@
 alter table device_ir_remote_commands
     add column remark text default null;
 alter table device_ir_remote_commands
-    drop column frequency;
+    drop column if exists frequency;
