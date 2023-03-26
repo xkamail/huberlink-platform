3f6172edff39ef080566eb7fd2052ee43c0ead59
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-27 00:23:21 +0700

    chore: a11y

--- web/app/head.tsx
+++ web/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>HuberLink</title>
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"
       />
       <meta name="description" content="HuberLink IoT Platform" />
       <link rel="icon" href="/favicon.ico" />
