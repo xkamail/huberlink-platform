096cfe51bc5ef6773b3225cd52603c0d089529d9
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-27 14:18:13 +0700

    chore: safe area

--- web/app/head.tsx
+++ web/app/head.tsx
@@ -6,6 +6,11 @@ export default function Head() {
         name="viewport"
         content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"
       />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
       <meta name="description" content="HuberLink IoT Platform" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="manifest" href="/manifest.json" />
