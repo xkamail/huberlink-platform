a0d70ddeedde3e7739a1ae58f59f0c8b7480bc33
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-30 20:21:16 +0700

    feat: create page

--- web/app/(home)/h/[id]/automation/create/page.tsx
+++ web/app/(home)/h/[id]/automation/create/page.tsx
@@ -1,14 +1,37 @@
 'use client'
 
+import Card from '@/components/ui/card'
+import Form from '@/components/ui/form'
 import PageHeader from '@/components/ui/page-header'
 import { useHomeSelector } from '@/lib/contexts/HomeContext'
+import { toSWR } from '@/lib/utils'
+import DeviceService from '@/services/DeviceService'
+import { useForm } from 'react-hook-form'
+import useSWR from 'swr'
 
 const CreateScenePage = () => {
+  const ctx = useForm()
   const homeId = useHomeSelector((s) => s.homeId)
+  const {
+    data: devices,
+    error: devicesError,
+    isLoading: devicesLoading,
+  } = useSWR(
+    ['home/automtion/create/devices', homeId],
+    toSWR(DeviceService.list(homeId))
+  )
+  const submit = () => {
+    //
+  }
   //
   return (
     <div>
       <PageHeader title="Create Automation" />
+      <Card>
+        <Form ctx={ctx} onSubmit={submit}>
+          <div></div>
+        </Form>
+      </Card>
     </div>
   )
 }
