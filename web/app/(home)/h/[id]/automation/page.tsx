f0c985f47e164854fd51eaaccd2b9a3adc15bae8
Pakorn Sangpeth <camel4882@gmail.com>
2023-03-30 20:17:38 +0700

    feat: scene list

--- web/app/(home)/h/[id]/automation/page.tsx
+++ web/app/(home)/h/[id]/automation/page.tsx
@@ -1,17 +1,21 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { TypographyH2 } from '@/components/ui/h2'
+import Spinner from '@/components/ui/spinner'
 import { useHomeSelector } from '@/lib/contexts/HomeContext'
 import { toSWR } from '@/lib/utils'
 import HomeService from '@/services/HomeService'
 import { PlusIcon } from 'lucide-react'
 import useSWR from 'swr'
+
 const AutomationPage = () => {
   const homeId = useHomeSelector((s) => s.homeId)
   const { data, error, isLoading } = useSWR(
     ['home/automtion', homeId],
     toSWR(HomeService.scenes.list(homeId))
   )
+  if (error) return <div>error</div>
+
   return (
     <div>
       <div className="mb-4 flex justify-between items-center">
@@ -21,7 +25,11 @@ const AutomationPage = () => {
         </Button>
       </div>
       <div>
-        {isLoading && <div>Loading...</div>}
+        {isLoading && (
+          <div className="flex justify-center py-10">
+            <Spinner />
+          </div>
+        )}
 
         {data && data.map((scene) => <div key={scene.id}>{scene.name}</div>)}
       </div>
